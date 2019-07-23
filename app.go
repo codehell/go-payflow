e4dc7e61220438fcb6ff44fd0c6ba7c4aeb9f5b6
dmorales <dmorales@inespay.com>
2019-07-23 17:08:09 +0200

    gin with session

--- app.go
+++ app.go
@@ -1,12 +1,16 @@
 package main
 
 import (
-	"cloud.google.com/go/firestore"
 	"context"
 	"encoding/base64"
-	"github.com/gin-gonic/gin"
-	"google.golang.org/api/iterator"
 	"log"
+
+	"cloud.google.com/go/firestore"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/gin-gonic/gin"
+	csrf "github.com/utrack/gin-csrf"
+	"google.golang.org/api/iterator"
 )
 
 // ProjectID name of the project
@@ -16,20 +20,22 @@ var ctx = context.Background()
 
 func main() {
 	r := gin.Default()
-	r.GET("/decode/:token", func(c *gin.Context) {
-		encoded := c.Params.ByName("token")
-		decoded, err := base64.StdEncoding.DecodeString(encoded)
-		if err != nil {
-			c.JSON(500, gin.H{
-				"error": err.Error(),
-			})
-		}
+	store := cookie.NewStore([]byte("secret"))
+	options := sessions.Options{HttpOnly: true}
+	store.Options(options)
+	r.Use(sessions.Sessions("mysession", store))
 
-		c.JSON(200, gin.H{
-			"result": string(decoded),
-		})
-	})
+	r.Use(csrf.Middleware(csrf.Options{
+		Secret: "secret123",
+		ErrorFunc: func(c *gin.Context) {
+			c.String(400, "CSRF token mismatch")
+			c.Abort()
+		},
+	}))
+
+	r.GET("/decode/:token", decodeToken)
 	r.GET("/restaurants", getRestaurants)
+	r.GET("/tests", tests)
 	_ = r.Run()
 }
 
@@ -51,4 +57,33 @@ func getRestaurants(c *gin.Context) {
 		}
 	}
 	c.JSON(200, arry)
+}
+
+func tests(c *gin.Context) {
+	session := sessions.Default(c)
+	var count int
+	v := session.Get("count")
+	if v == nil {
+		count = 0
+	} else {
+		count = v.(int)
+		count++
+	}
+	session.Set("count", count)
+	session.Save()
+	c.JSON(200, gin.H{"count": count})
+}
+
+func decodeToken(c *gin.Context) {
+	encoded := c.Params.ByName("token")
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		c.JSON(500, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	c.JSON(200, gin.H{
+		"result": string(decoded),
+	})
 }
--- go.mod
+++ go.mod
@@ -4,15 +4,16 @@ go 1.12
 
 require (
 	cloud.google.com/go v0.41.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/sessions v0.0.0-20190512062852-3cb4c4f2d615
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.4.0
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mattn/go-isatty v0.0.8 // indirect
-	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/ugorji/go v1.1.7 // indirect
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
+	github.com/utrack/gin-csrf v0.0.0-20190424104817-40fb8d2c8fca
 	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
 	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
-	golang.org/x/tools v0.0.0-20190702152245-7e72c71c505f // indirect
 	google.golang.org/api v0.7.0
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
